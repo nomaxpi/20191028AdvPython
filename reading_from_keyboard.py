739cb753f392e029c4048c5fa193378329da4c69
jstrickler <jstrickler@gmail.com>
2019-10-28 15:34:05 -0400

    user input

--- reading_from_keyboard.py
+++ reading_from_keyboard.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+from getpass import getpass
 
 user_name = input("What is your name? ")
 print("Hello,", user_name)
+
+user_password = getpass("What is your password? ")
+print("password is:", user_password)
